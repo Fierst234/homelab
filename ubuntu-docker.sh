9966d7a3dca8ffe49686da4029ce117c7c254baf
Fierst234 <agent6545609@gmail.com>
2025-12-12 10:22:12 +0300

    Renamed and added auto confirm of installation

--- ubuntu-docker.sh
+++ ubuntu-docker.sh
@@ -20,4 +20,4 @@ EOF
 sudo apt update
 
 VERSION_STRING=5:29.1.2-1~ubuntu.24.04~noble
-sudo apt install docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y docker-ce=$VERSION_STRING docker-ce-cli=$VERSION_STRING containerd.io docker-buildx-plugin docker-compose-plugin
